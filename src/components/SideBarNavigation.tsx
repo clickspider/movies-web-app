4f924d96d17f98eeac6f7c557260c52dcd8a4b6e
clickspider <danielfrey101@gmail.com>
2022-04-06 15:57:37 +0200

    feat(firebase): Support UserAvatar

--- src/components/SideBarNavigation.tsx
+++ src/components/SideBarNavigation.tsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import icons from "../assets/icons";
+import { useSelector } from "react-redux";
+import { userSelector } from "../store/user/userSlice";
+const SideBarNavigation = () => {
+  const { user } = useSelector(userSelector);
 
-const SideBarNavigation = () => (
-  <div className="sidenav">
-    <icons.logo className="sidenav__logo" />
-    <nav className="sidenav__items">
-      <NavLink to="/">
-        <icons.iconNavHome />
-      </NavLink>
-      <NavLink to="/movies-catalog">
-        <icons.iconNavMovies />
-      </NavLink>
-      <NavLink to="/TV-catalog">
-        <icons.iconNavTVSeries />
-      </NavLink>
-      <NavLink to="/bookmarked">
-        <icons.iconNavBookmark />
-      </NavLink>
-    </nav>
+  return (
+    <div className="sidenav">
+      <icons.logo className="sidenav__logo" />
+      <nav className="sidenav__items">
+        <NavLink to="/">
+          <icons.iconNavHome />
+        </NavLink>
+        <NavLink to="/movies-catalog">
+          <icons.iconNavMovies />
+        </NavLink>
+        <NavLink to="/TV-catalog">
+          <icons.iconNavTVSeries />
+        </NavLink>
+        <NavLink to="/bookmarked">
+          <icons.iconNavBookmark />
+        </NavLink>
+      </nav>
 
-    <img src={icons.avatar} alt="avatar" className="sidenav__avatar" />
-  </div>
-);
+      <img
+        src={user.profile?.photoURL || icons.avatar}
+        alt="avatar"
+        className="sidenav__avatar"
+      />
+    </div>
+  );
+};
 
 export default SideBarNavigation;
