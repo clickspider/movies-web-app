e3cc48fcb6329c7f6026e61a509e566dd64cf366
clickspider <danielfrey101@gmail.com>
2022-04-22 08:57:30 +0200

    feat(storybook): Button Cmp - done

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,36 +2,36 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Button from "../components/Button";
-import icons from "../assets/icons";
+import iconGoogle from "../assets/icon-google.svg";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Button",
+  title: "Button",
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  argTypes: {},
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = (args: any) => {
+export const PrimaryWithIcon = (args: any) => {
   return (
     <Template {...args}>
-      <icons.iconGoogle />
+      <img src={iconGoogle} width="30" height="30" alt="google-icon" />
+      <span style={{ marginLeft: 10 }}>Login with google</span>
     </Template>
   );
-};
-
-export const Icon = Template.bind({});
-Icon.args = {
-  children: <icons.iconGoogle />,
 };
 
 export const Loading = Template.bind({});
 Loading.args = {
   isLoading: true,
-  title: "Button",
+  title: "Loading...",
+  backgroundColor: "hsl(0deg, 97%, 63%)",
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  title: "Click here",
 };
