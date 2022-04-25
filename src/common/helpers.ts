d3ac5fd48975f8343b99f60605e1181701b79ff0
clickspider <danielfrey101@gmail.com>
2022-04-25 15:08:54 +0200

    feat(storybook): added new helper func

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,3 +1,7 @@
+import { addDoc, collection, db } from "./fbConfig";
+import { Movie } from "../store/movies/types";
+import data from "./data.json";
+
 export const isMovieBookmarked = (
   movieIdx: number,
   bookmarkedIdsArray: number[]
@@ -5,3 +9,36 @@ export const isMovieBookmarked = (
   const isBookmarked = bookmarkedIdsArray.includes(movieIdx);
   return isBookmarked;
 };
+
+export const addMockDataToFirestone = (function () {
+  const {
+    movies: { moviesList },
+  }: { movies: { moviesList: Movie[] } } = JSON.parse(JSON.stringify(data));
+  let executed = false;
+  return function () {
+    if (!executed) {
+      executed = true;
+      moviesList.forEach(async function ({
+        title,
+        id,
+        thumbnail,
+        year,
+        category,
+        rating,
+        isBookmarked,
+        isTrending,
+      }) {
+        return await addDoc(collection(db, "moviesList"), {
+          title,
+          id,
+          thumbnail,
+          year,
+          category,
+          rating,
+          isBookmarked,
+          isTrending,
+        });
+      });
+    }
+  };
+})();
